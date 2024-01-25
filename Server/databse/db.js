d43bbcd69aaf3fe2da82d827212cbc99b625300d
Ayush <ayushsingh1771@gmail.com>
2024-01-25 11:47:32 +0530

    api-update

--- Server/databse/db.js
+++ Server/databse/db.js
@@ -4,8 +4,8 @@ const Connections = async () => {
     const DB_URI = `mongodb://gmailClone:7291019063@ac-spnm3qa-shard-00-00.f550bnp.mongodb.net:27017,ac-spnm3qa-shard-00-01.f550bnp.mongodb.net:27017,ac-spnm3qa-shard-00-02.f550bnp.mongodb.net:27017/?ssl=true&replicaSet=atlas-k2h3ce-shard-0&authSource=admin&retryWrites=true&w=majority`; // Your MongoDB connection URI
     try {
         await mongoose.connect(DB_URI, {
-            useNewUrlParser: true, // Deprecated, but still needed for older versions
-            useUnifiedTopology: true, // Use the new topology engine
+            useNewUrlParser: true,
+            useUnifiedTopology: true,  
         });
         console.log('Database is Connected ............');
     } catch (error) {
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://gmail-clone-blue.vercel.app/';
 
 const API = async(urlObject,payload , type)=>{
    return await axios({
