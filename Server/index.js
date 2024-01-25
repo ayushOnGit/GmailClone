619bceb696de7ecb0fe7fb8086f3fe50a429fee1
Ayush <ayushsingh1771@gmail.com>
2024-01-25 11:55:08 +0530

    backend cors solved

--- Server/index.js
+++ Server/index.js
@@ -3,16 +3,19 @@ const Connections = require("./databse/db.js");
 const routes = require("./Routes/route.js");
 const cors = require('cors');
 
-
-
 const app = express();
 
+// Allow requests from your frontend domain
+const corsOptions = {
+  origin: 'https://gmail-clone-74y9.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
 
-
-app.use(cors());
-app.use(express.urlencoded({extended : true}));
-app.use(express.json({extended : true}));
-app.use('/',routes);
+app.use(cors(corsOptions));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json({extended: true}));
+app.use('/', routes);
 
 const PORT = 8000;
 Connections();
@@ -21,6 +24,6 @@ app.listen(PORT, (error) => {
   if (error) {
     console.error("Error starting the server:", error);
   } else {
-    console.log(`Server is Connected on port ${PORT}`);
-  }
+    console.log(`Server is Connected on port ,${PORT}`);
+  }
 });
