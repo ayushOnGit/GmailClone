1d0cfa5bf0faba3bd21cdfe22e5f5d058dbdb26d
Ayush <ayushsingh1771@gmail.com>
2024-01-25 12:39:11 +0530

    commit-cors

--- Server/index.js
+++ Server/index.js
@@ -17,12 +17,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
 
 // Additional CORS headers in route handling middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://gmail-clone-74y9.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://gmail-clone-74y9.vercel.app');
+//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 app.use('/', routes);
 
